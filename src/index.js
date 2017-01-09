10355d238ee694eeb1492c98c9acf4a4ef981c0e
mlampedx <mlampesd@gmail.com>
2017-01-08 20:57:40 -0800

    Added babel-polyfill

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
+import 'babel-polyfill';
 import { render } from 'react-dom';
 import { createStore, dispatch } from 'redux';
 import { Provider } from 'react-redux';
 import rootReducer from './reducers/index';
 import App from './components/App';
-
-let store = createStore(rootReducer);
+import store from './store';
 
 render(
   <Provider store = { store }>
