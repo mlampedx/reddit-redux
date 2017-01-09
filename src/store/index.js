3b6ff972ca84187788fa27697bd45dc7b0a56069
mlampedx <mlampesd@gmail.com>
2017-01-08 20:58:04 -0800

    Created index file for store

---
+++ src/store/index.js
@@ -0,0 +1,11 @@
+import thunkMiddleware from 'redux-thunk';
+import { createStore, applyMiddleware } from 'redux';
+import { selectSubreddit, fetchPosts } from './../actions';
+import rootReducer from './../reducers';
+
+export const store = createStore(rootReducer, applyMiddleware(rootReducer));
+
+store.dispatch(selectSubreddit('reactjs'));
+store.dispatch(fetchPosts('reactjs')).then(() => {
+  console.log(store.getState());
+});
