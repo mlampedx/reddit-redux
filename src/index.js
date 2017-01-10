0798bf1c59cd9924a46f5bca8345af9d9861ff39
mlampedx <mlampesd@gmail.com>
2017-01-09 19:22:57 -0800

    Mounted app on react-dom

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { render } from 'react-dom';
 import { createStore, dispatch } from 'redux';
 import { Provider } from 'react-redux';
 import rootReducer from './reducers/index';
-import App from './components/App';
+import App from './components';
 import store from './store';
 
 render(
