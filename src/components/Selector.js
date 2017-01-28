57c11b14b05e04ef7daa855f9761cab1d4d78e2c
mlampedx <mlampesd@gmail.com>
2017-01-27 19:49:51 -0800

    Refactored select elements options mapping to one-line arrow function

--- src/components/Selector.js
+++ src/components/Selector.js
@@ -3,14 +3,9 @@ import React, { PropTypes } from 'react';
 export default function Selector({value, onChange, options}) {
   return (
     <span>
-    <h1>{value}</h1>
       <select onChange = {e => onChange(e.target.value)}
               value = {value}>
-        {options.map(option => {
-          <option value = {option} key = {option}>
-            {option}
-          </option>
-        })}
+        {options.map(option => <option value = {option} key = {option}>{option}</option>)}
       </select>
     </span>
   )
