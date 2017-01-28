7b208cf13e39450f90113662e8d4ca0d13ccb81e
mlampedx <mlampesd@gmail.com>
2017-01-27 19:50:26 -0800

    Wrapped provided with MuiThemeProvider

--- src/index.js
+++ src/index.js
@@ -3,13 +3,17 @@ import 'babel-polyfill';
 import { render } from 'react-dom';
 import { createStore, dispatch } from 'redux';
 import { Provider } from 'react-redux';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import rootReducer from './reducers/index';
-import App from './components';
+import App from './components/App';
 import store from './store';
+require('./styles.scss');
 
 render(
-  <Provider store = { store }>
-    <App />
-  </Provider>,
+  <MuiThemeProvider>
+    <Provider store = { store }>
+      <App />
+    </Provider>
+  </MuiThemeProvider>,
   document.getElementById('app')
 );
